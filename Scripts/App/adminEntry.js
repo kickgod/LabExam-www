a1cfb8c1ecf45fd33863450c886a3c3993f0d5da
JxKicker <1427035242@qq.com>
2018-10-30 18:58:26 +0800

    change many

--- Scripts/App/adminEntry.js
+++ Scripts/App/adminEntry.js
@@ -4,6 +4,7 @@ $(function(){
         let winHeights =[$(window).height(),$(document).height(),$(document.body).height(),$(document.body).outerHeight(true)];
         var maxHeight = winHeights.sort((one,two)=> one < two)[0];        
         $('.layout-height-100').height(maxHeight);
+        return maxHeight;
     }
     $(window).on('resize',null,null,set_layout_height_100);
     set_layout_height_100();  
@@ -29,8 +30,48 @@ $(function(){
         return `${$(this).attr('data-max-width')}px`;
     });
 
+    /* 设置最小高度 */ 
+    $('[data-min-height]').css('min-height',function(index,width){
+        return `${$(this).attr('data-min-height')}px`;
+    });
+    /* 设置最大高度 */ 
+    $('[data-max-height]').css('max-height',function(index,width){
+        return `${$(this).attr('data-max-height')}px`;
+    });
+
+
     $('[data-margin-top]').css('margin-top',function(index,width){
         return `${$(this).attr('data-margin-top')}px`;
     });
 
+    $('[data-margin-bottom]').css('margin-bottom',function(index,width){
+        return `${$(this).attr('data-margin-bottom')}px`;
+    });
+
+    $(window).on('resize',null,null,function(){
+        $('[data-height-all]').css('height',function(index,width){
+            let val = set_layout_height_100() - $('.admin-narbar-top').height() - 20;
+            return `${val}px`;
+        });
+    });
+    
+    $('[data-height-all]').css('height',function(index,width){
+        let val = set_layout_height_100() - $('.admin-narbar-top').height() - 20;
+        return `${val}px`;
+    });
+
+    $('#admin-only-show-505').on('click',null,null,function(){
+        var content =$('#admin-only-show-505-content');
+
+        if(content.css('display') == "block"){
+            content.css("display","none");
+        }else{
+            content.css("display","block");
+        }
+        
+    });
+
+    $('#admin-only-show-505-content-cancel').on('click',null,null,function(){
+        $('#admin-only-show-505-content').css("display","none");
+    });
 });
